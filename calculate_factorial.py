9de8559b308888682994d123c3eff10fe2792518
BernaJArul <aruldhasjbc_bec28@mepcoeng.ac.in>
2026-04-16 01:07:28 +0530

    Add factorial calculation program in Python

---
+++ calculate_factorial.py
@@ -0,0 +1,7 @@
+# Python program to find the factorial of a no.
+
+num = int(input("Enter a number:"))
+fact = 1
+for i in range(1,num+1):
+   fact = (fact * i)
+print("Factorial of",num,"is:",fact)
