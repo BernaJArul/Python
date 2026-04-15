1f15855b7462fabb0ad9a00b97890478869b5598
BernaJArul <aruldhasjbc_bec28@mepcoeng.ac.in>
2026-04-16 01:06:37 +0530

    Add prime number check program
    Implement a program to check if a number is prime.

---
+++ check_prime_or_not.py
@@ -0,0 +1,11 @@
+# Python program to check whether a no. is prime or not
+
+num = int(input("Enter a number:"))
+count = 0
+for i in range(1,num+1):
+   if num%i == 0:
+      count = count+1
+if count == 2:
+   print(num,"is prime number")
+else:
+   print(num,"is not a prime number")
