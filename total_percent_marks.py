35e23188289858264dd09bf2aeec47a417001668
BernaJArul <aruldhasjbc_bec28@mepcoeng.ac.in>
2026-04-16 01:00:54 +0530

    Add program to calculate total and percentage of marks

---
+++ total_percent_marks.py
@@ -0,0 +1,9 @@
+# Python program to find total and percentage of marks
+m1 = float(input("Enter m1:"))
+m2 = float(input("Enter m2:"))
+m3 = float(input("Enter m3:"))
+m4 = float(input("Enter m4:"))
+tot = m1+m2+m3+m4
+avg = tot/4
+print("Total is:",tot)
+print("Percentage is:",avg,"%")
