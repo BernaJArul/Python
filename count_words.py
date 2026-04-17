412fcb3b4325413b13ab789219462bd2ac759e2f
BernaJArul <aruldhasjbc_bec28@mepcoeng.ac.in>
2026-04-17 18:17:57 +0530

    Add word counting functionality in count_words.py

---
+++ count_words.py
@@ -0,0 +1,7 @@
+# Python program to count the no. of words in a sentence
+
+s = input("Enter a sentence:")
+
+# Function to split up the words and then count it
+c = len(s.split())
+print("No. of words in the given sentence is:",c)
