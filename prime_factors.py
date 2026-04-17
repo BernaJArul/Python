b5c9d6fbf4f51405d8ff5337609b681f96c88baf
BernaJArul <aruldhasjbc_bec28@mepcoeng.ac.in>
2026-04-17 18:10:09 +0530

    Implement prime factorization function in Python
    Added a function to compute prime factors of a number.

---
+++ prime_factors.py
@@ -0,0 +1,20 @@
+# Python function to find the prime factors of a given number
+
+# Function to find the prime factors 
+import math
+def prime_factors(n):
+   while n%2==0:
+      print(2)
+      n = n//2
+
+   for i in range(3,int(math.sqrt(n))+1,2):
+      while n%i==0:
+       print(i)
+      n = n//i
+   
+   if n>2:
+      print(n)
+
+# Main routine
+num = int(input("Enter a number:"))
+prime_factors(num)
