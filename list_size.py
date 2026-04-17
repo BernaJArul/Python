11ebf20b872defe3581f2740816c3213ac298447
BernaJArul <aruldhasjbc_bec28@mepcoeng.ac.in>
2026-04-17 18:21:50 +0530

    Add function to store colors and return list size

---
+++ list_size.py
@@ -0,0 +1,8 @@
+# Function to store the names of colors and return the size of list
+def store_colors():
+   colors = ['red','blue','green','yellow','purple','orange']
+   return len(colors)
+
+# Main Routine
+color_list_size = store_colors()
+print(f"size of color list:{color_list_size}")
