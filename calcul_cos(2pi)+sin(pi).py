3931f14f7090fbe6ada2450092dd5bbdcc23b01a
BernaJArul <aruldhasjbc_bec28@mepcoeng.ac.in>
2026-04-27 11:23:35 +0530

    Add program to calculate cos(2pi) + sin(pi)

---
+++ calcul_cos(2pi)+sin(pi).py
@@ -0,0 +1,14 @@
+#Python program to calculate cos(2pi)+sin(pi) using modules
+
+import math
+
+# Calculate cos(2pi)
+cos_2pi = math.cos(2 * math.pi)
+
+# Calculate sin(pi)
+sin_pi = math.sin(math.pi)
+
+# Calculate the sum
+result = cos_2pi + sin_pi
+
+print(result)
